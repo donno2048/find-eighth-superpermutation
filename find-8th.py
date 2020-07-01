95247d226cb5673f3fa7b472c45217027a3e22f3
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-07-01 12:47:16 +0300

    Update find-8th.py

--- find-8th.py
+++ find-8th.py
@@ -1,3 +1,3 @@
 from itertools import permutations;c=list(permutations([str(k) for k in range(8)],8));i=8**46077
-while any([''.join(j) not in oct(i).replace('o','') for j in c]):i+=1
+while any([''.join(j) not in oct(i).replace('o','1234567') for j in c]):i+=1
 print(''.join([str(int(i)+1) for i in oct(i).replace('o','')]))
