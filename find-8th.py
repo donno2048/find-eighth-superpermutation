9c1b1a84b1f2aac4e5f5b0bc3ed8feb57af38504
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-07-01 12:15:46 +0300

    Create find-8th.py

---
+++ find-8th.py
@@ -0,0 +1,3 @@
+from itertools import permutations;c=list(permutations([str(k) for k in range(8)],8));i=8**2**10
+while any([''.join(j) not in oct(i).replace('o','') for j in c]):i+=1
+print(''.join([str(int(i)+1) for i in oct(i).replace('o','')]))
